88c642955a93e8681f83842d012169dac26cafa8
shubham3619 <120643298+shubham3619@users.noreply.github.com>
2023-07-20 02:35:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+window.addEventListener('DOMContentLoaded', (event) => {
+  const selectElement = document.getElementById('colorSelect');
+  const removeButton = document.querySelector('input[type="button"]');
+  
+  removeButton.addEventListener('click', () => {
+    const selectedIndex = selectElement.selectedIndex;
+    if (selectedIndex !== -1) {
+      selectElement.remove(selectedIndex);
+    }
+  });
+});
+
